546a1c3bb101d3f4cff6a9b8796c4a945e2367e0
HoiAn2010 <Alinesonza@aol.com>
2022-10-19 00:56:22 +0200

    slider begun

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 /*menu burger*/
 
 
-/*slide*/
+/*slider*/
+let imageOfSlider = document.getElementsByClassName("img-slider");
+
+let moveImage = 0;
+
+let numberOfImage = imageOfSlider.length;
+
+let precedent = document.querySelector('.precedent');
+
+let suivant = document.querySelector ('.suivant');
+
+function enleverImageActive(){
+    for (let i = 0; i < imageOfSlider.length; i++){
+    imageOfSlider[i].classList.remove('active');
+    }
+}
+
+suivant.addEventListener('click', function() {
+    moveImage++;
+    enleverImageActive();
+    imageOfSlider[moveImage].classList.add('active');
+    }
+)
+/* reprendre à 13:12 de la vidéo Ben*/
 
 
-
-/*bouton qui change de couleur au click */
+/*bouton qui change couleur au click */
 
 /*Merci pour votre message. Je reviendrai vers vous sous 48h. Namaste*/
